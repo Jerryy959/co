f942257a45151040975bc4382bbfd523acd1eec4
jerryy959 <1376723538@qq.com>
2025-02-12 00:17:44 +0800

    fix dim

--- rl_test.py
+++ rl_test.py
@@ -8,7 +8,7 @@ from Network import ActorNetwork
 from torch.distributions import Categorical
 
 
-S_INFO = 6  # bit_rate, buffer_size, next_chunk_size, bandwidth_measurement(throughput and time), chunk_til_video_end
+S_INFO = 5  # bit_rate, buffer_size, next_chunk_size, bandwidth_measurement(throughput and time), chunk_til_video_end
 S_LEN = 8  # take how many frames in the past
 A_DIM = 6
 ACTOR_LR_RATE = 0.0001
@@ -97,7 +97,7 @@ def main():
         state[2, -1] = float(video_chunk_size) / float(delay) / M_IN_K  # kilo byte / ms
         state[3, -1] = float(delay) / M_IN_K / BUFFER_NORM_FACTOR  # 10 sec
         state[4, :A_DIM] = torch.tensor(next_video_chunk_sizes) / M_IN_K / M_IN_K  # mega byte
-        state[5, -1] = min(video_chunk_remain, CHUNK_TIL_VIDEO_END_CAP) / float(CHUNK_TIL_VIDEO_END_CAP)
+        #state[5, -1] = min(video_chunk_remain, CHUNK_TIL_VIDEO_END_CAP) / float(CHUNK_TIL_VIDEO_END_CAP)
 
         with torch.no_grad():
             probability=net.forward(state.unsqueeze(0))
